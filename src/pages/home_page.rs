b4fb86c42791409d442675775136f3ad991bf754
Baptiste de Montangon <baptiste.demontangon@gmail.com>
2023-07-17 20:30:16 +0200

    added board comp to view

--- src/pages/home_page.rs
+++ src/pages/home_page.rs
@@ -1,3 +1,5 @@
+use crate::components::board::DrawBoard;
+use hex_chess_core::board::Board;
 use leptos::*;
 use leptos_meta::*;
 
@@ -6,11 +8,15 @@ use leptos_meta::*;
 pub fn HomePage(cx: Scope) -> impl IntoView {
     // Creates a reactive value to update the button
     let (count, set_count) = create_signal(cx, 0);
+    let (board, _set_board) = create_signal(cx, Board::new());
     let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! { cx,
         <Title text="Hex Chess | Home"/>
         <h1>"Welcome to Hex chess!"</h1>
         <button on:click=on_click>"Click Me: " {count}</button>
+        <div class="board">
+            <DrawBoard board/>
+        </div>
     }
 }
