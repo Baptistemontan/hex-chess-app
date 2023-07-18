71bdc4e099e6f073040e8651036a5cccd3b18c89
Baptiste de Montangon <baptiste.demontangon@gmail.com>
2023-07-18 19:14:27 +0200

    remove useless counter + centered the board

--- src/pages/home_page.rs
+++ src/pages/home_page.rs
@@ -7,14 +7,11 @@ use leptos_meta::*;
 #[component]
 pub fn HomePage(cx: Scope) -> impl IntoView {
     // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(cx, 0);
     let (board, update_board) = create_signal(cx, Board::new());
-    let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! { cx,
         <Title text="Hex Chess | Home"/>
         <h1>"Welcome to Hex chess!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
         <div class="board">
             <DrawBoard board update_board/>
         </div>
