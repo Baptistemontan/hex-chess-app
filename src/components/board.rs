f26c3cb59c5c80606742283c4e2471bd76e5da5e
Baptiste de Montangon <baptiste.demontangon@gmail.com>
2023-07-20 02:39:53 +0200

    can now promote

--- src/components/board.rs
+++ src/components/board.rs
@@ -1,22 +1,42 @@
 use std::collections::HashSet;
 
-use hex_chess_core::{board::Board, hex_coord::HexVector, mov::MaybePromoteMove, piece::PieceKind};
+use hex_chess_core::{
+    board::Board as HexBoard,
+    hex_coord::HexVector,
+    mov::{CanPromoteMove, MaybePromoteMove},
+    piece::{Color as PieceColor, Piece, PieceKind},
+};
 use leptos::*;
 
 // use leptos_meta::*;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum Color {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum HexColor {
     Black,
     Grey,
     White,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Orientation {
+    Normal,
+    Reversed,
+}
+
+impl Orientation {
+    pub fn reverse_assign(&mut self) {
+        match self {
+            Orientation::Normal => *self = Orientation::Reversed,
+            Orientation::Reversed => *self = Orientation::Normal,
+        }
+    }
 }
 
 struct GridIterator {
     q: isize,
     r: isize,
     line: isize,
-    colors: [Color; 3],
+    colors: [HexColor; 3],
 }
 
 impl GridIterator {
@@ -25,13 +45,13 @@ impl GridIterator {
             q: -5,
             r: -2,
             line: 0,
-            colors: [Color::Black, Color::Grey, Color::White],
+            colors: [HexColor::Black, HexColor::Grey, HexColor::White],
         }
     }
 }
 
 impl Iterator for GridIterator {
-    type Item = (HexVector, Color);
+    type Item = (HexVector, HexColor);
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.line >= 11 {
@@ -56,52 +76,71 @@ impl Iterator for GridIterator {
     }
 }
 
+fn get_piece_url(piece: Piece) -> String {
+    let color = match piece.color {
+        PieceColor::Black => 'b',
+        PieceColor::White => 'w',
+    };
+    let kind = match piece.kind {
+        PieceKind::OriginalPawn | PieceKind::Pawn => 'p',
+        PieceKind::Knight => 'n',
+        PieceKind::Bishop => 'b',
+        PieceKind::Rook => 'r',
+        PieceKind::King => 'k',
+        PieceKind::Queen => 'q',
+    };
+    format!("/assets/pieces/{color}{kind}.png")
+}
+
+#[allow(clippy::too_many_arguments)]
 fn hexagon<F>(
     cx: Scope,
     vector: HexVector,
-    color: Color,
-    board: ReadSignal<Board>,
+    color: HexColor,
+    board: ReadSignal<HexBoard>,
     selected: ReadSignal<Option<HexVector>>,
     on_select: F,
     legal_moves: Memo<Option<HashSet<MaybePromoteMove>>>,
+    orientation: ReadSignal<Orientation>,
+    can_promote: ReadSignal<Option<CanPromoteMove>>,
 ) -> impl IntoView
 where
-    F: Fn(HexVector) + 'static,
+    F: Fn(HexVector, Option<PieceKind>) + Copy + 'static,
 {
-    let hide = vector.mag() > 5;
+    let vector = move || {
+        if orientation.get() == Orientation::Normal {
+            vector
+        } else {
+            -vector
+        }
+    };
+    let hide = move || vector().mag() > 5;
 
-    let piece = create_memo(cx, move |_| board.get().get_piece_at(vector));
+    let piece = create_memo(cx, move |_| board.get().get_piece_at(vector()));
 
-    let piece_image_url = create_memo(cx, move |_| {
-        piece.get().map(|piece| {
-            use hex_chess_core::piece::Color;
-            let color = match piece.color {
-                Color::Black => 'b',
-                Color::White => 'w',
-            };
-            let kind = match piece.kind {
-                PieceKind::OriginalPawn | PieceKind::Pawn => 'p',
-                PieceKind::Knight => 'n',
-                PieceKind::Bishop => 'b',
-                PieceKind::Rook => 'r',
-                PieceKind::King => 'k',
-                PieceKind::Queen => 'q',
-            };
-            format!("/assets/pieces/{color}{kind}.png")
-        })
-    });
+    let piece_image_url = create_memo(cx, move |_| piece.get().map(get_piece_url));
 
-    let selected = move || selected.get().is_some_and(|pos| pos == vector);
+    let selected = move || selected.get().is_some_and(|pos| pos == vector());
 
     let is_move_dest = create_memo(cx, move |_| {
         legal_moves
             .get()
-            .is_some_and(|moves| moves.iter().any(|mov| mov.to() == vector))
+            .is_some_and(|moves| moves.iter().any(|mov| mov.to() == vector()))
     });
     let is_piece_and_dest = move || piece.get().is_some() && is_move_dest.get();
 
     let on_click = move |_| {
-        on_select(vector);
+        on_select(vector(), None);
+    };
+
+    let is_promote = move || {
+        can_promote.get().and_then(|promote_move| {
+            if promote_move.to != vector() {
+                None
+            } else {
+                Some(promote_move)
+            }
+        })
     };
 
     view! { cx,
@@ -113,9 +152,9 @@ where
         >
             <div
                 class="hex-grid__content"
-                class=("hex-grid__content__black", move || color == Color::Black && !selected())
-                class=("hex-grid__content__grey", move || color == Color::Grey && !selected())
-                class=("hex-grid__content__white", move || color == Color::White && !selected())
+                class=("hex-grid__content__black", move || color == HexColor::Black && !selected())
+                class=("hex-grid__content__grey", move || color == HexColor::Grey && !selected())
+                class=("hex-grid__content__white", move || color == HexColor::White && !selected())
                 class=("hex-grid__content__selected", selected)
                 class=("hex-grid__content__is_dest", move || is_move_dest.get() && !is_piece_and_dest())
                 class=("hex-grid__content__is_piece_and_dest", is_piece_and_dest)
@@ -127,33 +166,78 @@ where
                     }
                 })}
             </div>
+            {move || is_promote().map(move |promote_move| {
+                let promote_fn = move |piece_kind| on_select(vector(), Some(piece_kind));
+                promote(cx, orientation, promote_move.color, promote_fn)
+            })}
         </li>
     }
 }
 
-#[component]
-pub fn DrawBoard(
+fn promote_to<F>(cx: Scope, piece: Piece, promote_fn: F) -> impl IntoView
+where
+    F: Fn(PieceKind) + Copy + 'static,
+{
+    let on_click = move |_| promote_fn(piece.kind);
+    let piece_url = get_piece_url(piece);
+    view! { cx,
+        <img on:click=on_click class="piece_image" src=piece_url />
+    }
+}
+
+fn promote<F>(
     cx: Scope,
-    board: ReadSignal<Board>,
-    update_board: WriteSignal<Board>,
+    orientation: ReadSignal<Orientation>,
+    piece_color: PieceColor,
+    promote_fn: F,
+) -> impl IntoView
+where
+    F: Fn(PieceKind) + Copy + 'static,
+{
+    let is_reversed =
+        move || match (orientation.get(), piece_color) {
+            (Orientation::Normal, PieceColor::White)
+            | (Orientation::Reversed, PieceColor::Black) => false,
+            (Orientation::Normal, PieceColor::Black)
+            | (Orientation::Reversed, PieceColor::White) => true,
+        };
+    view! { cx,
+        <div class="promote" class=("promote__reversed", is_reversed)>
+        {
+            [PieceKind::Queen, PieceKind::Rook, PieceKind::Knight, PieceKind::Bishop]
+            .into_iter()
+            .map(|kind| Piece::new(piece_color, kind))
+            .map(|piece| promote_to(cx, piece, promote_fn))
+            .collect_view(cx)
+        }
+        </div>
+    }
+}
+
+#[component]
+fn DrawHexBoard(
+    cx: Scope,
+    board: RwSignal<HexBoard>,
+    orientation: ReadSignal<Orientation>,
 ) -> impl IntoView {
     let (selected, set_selected) = create_signal(cx, None);
-    let (_can_promote, set_can_promote) = create_signal(cx, None);
+    let (can_promote, set_can_promote) = create_signal(cx, None);
 
     let play_move = move |from: HexVector, to: HexVector, promote: Option<PieceKind>| {
-        update_board.update(|board| match board.play_move(from, to, promote) {
+        board.update(|board| match board.play_move(from, to, promote) {
             Ok(None) => set_selected.set(None),
             Ok(Some(promote)) => set_can_promote.set(Some(promote)),
             Err(err) => log!("invalid move: {:?}", err),
         })
     };
 
-    let on_select = move |pos: HexVector| {
+    let on_select = move |pos: HexVector, promote_to: Option<PieceKind>| {
         let (target_piece, turn) =
             board.with(|board| (board.get_piece_at(pos), board.get_player_turn()));
+        set_can_promote.set(None);
         match (selected.get(), target_piece) {
             (_, Some(piece)) if piece.color == turn => set_selected(Some(pos)),
-            (Some(selected), _) => play_move(selected, pos, None),
+            (Some(selected), _) => play_move(selected, pos, promote_to),
             (None, _) => (),
         }
     };
@@ -168,7 +252,21 @@ pub fn DrawBoard(
 
     view! { cx,
         <ul class="hex-grid__list">
-        {GridIterator::new().map(|(vector, color)| hexagon(cx, vector, color, board, selected, on_select, current_legal_moves)).collect_view(cx)}
+        {GridIterator::new().map(|(vector, color)| hexagon(cx, vector, color, board.read_only(), selected, on_select, current_legal_moves, orientation, can_promote)).collect_view(cx)}
         </ul>
+    }
+}
+
+#[component]
+pub fn Board(cx: Scope, board: RwSignal<HexBoard>) -> impl IntoView {
+    let (orientation, set_orientation) = create_signal(cx, Orientation::Normal);
+
+    let on_switch = move |_| set_orientation.update(Orientation::reverse_assign);
+
+    view! { cx,
+        <div>
+            <DrawHexBoard board orientation/>
+            <button on:click=on_switch>"switch side"</button>
+        </div>
     }
 }
--- src/pages/home_page.rs
+++ src/pages/home_page.rs
@@ -1,4 +1,4 @@
-use crate::components::board::DrawBoard;
+use crate::components::board::Board;
 use hex_chess_core::board::Board;
 use leptos::*;
 use leptos_meta::*;
@@ -7,13 +7,13 @@ use leptos_meta::*;
 #[component]
 pub fn HomePage(cx: Scope) -> impl IntoView {
     // Creates a reactive value to update the button
-    let (board, update_board) = create_signal(cx, Board::new());
+    let board = create_rw_signal(cx, Board::new());
 
     view! { cx,
         <Title text="Hex Chess | Home"/>
         <h1>"Welcome to Hex chess!"</h1>
         <div class="board">
-            <DrawBoard board update_board/>
+            <Board board/>
         </div>
     }
 }
