b399baa0764ff45a467165b959b446fe6975b594
Baptistemontan <baptiste.demontangon@gmail.com>
2023-08-08 00:46:10 +0200

    updated leptos_i18n

--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -5,7 +5,6 @@ pub mod play;
 use crate::i18n::Locales;
 use home_page::HomePage;
 use leptos::*;
-use leptos_i18n::I18nContextProvider;
 use leptos_meta::*;
 use leptos_router::*;
 use not_found::NotFound;
@@ -19,13 +18,14 @@ pub fn App(cx: Scope) -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context(cx);
 
+    leptos_i18n::provide_i18n_context::<Locales>(cx);
+
     view! { cx,
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/hex-chess-app.css"/>
         <Link rel="manifest" href="/manifest.json"/>
         <Meta name="description" content="Website to play hexagonal chess, solo or with friends." />
-        <I18nContextProvider locales=Locales>
         <AuthentificationContext>
             <Router>
                 <Layout>
@@ -40,6 +40,5 @@ pub fn App(cx: Scope) -> impl IntoView {
                     </Layout>
             </Router>
         </AuthentificationContext>
-        </I18nContextProvider>
     }
 }
